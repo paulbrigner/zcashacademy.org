4239cf8a51b595b3b723642d1a32864bbde1d95a
Paul Brigner <paul@paulbrigner.com>
2025-08-05 02:20:51 +0530

    chore: output build to out

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   trailingSlash: true, // Ensures compatibility with S3/CloudFront
   basePath: '/community', // Sets the subdirectory path
   assetPrefix: '/community/',
+  distDir: 'out',
 };
 
 export default nextConfig;
